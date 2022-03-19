33c7eb26648be86dde96452ce6697e266e581e58
Aditya <adityacse063@gmail.com>
2022-03-19 15:22:38 +0530

    changes

--- js/custom.js
+++ js/custom.js
@@ -6,8 +6,8 @@ jQuery(document).ready(function () {
     //this code is for the gmap
     var map = new GMaps({
         el: '#map',
-        lat: -12.043333,
-        lng: -77.028333
+        lat: 86.15124347410861,
+        lng: 23.665638730659726
     });
 
 
